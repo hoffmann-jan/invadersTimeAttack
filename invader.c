48dcb5c719b5747d546744935c2379c08ed21ce8
Tino Teichgraeber <tinoteichgraeber@gmail.com>
2018-11-22 12:03:58 +0100

    bug

--- invader.c
+++ invader.c
@@ -12,6 +12,11 @@
 /* need kbhit implementation */
 #include "myconio.h"
 
+/**/
+#include "List.h"
+#include "Entity.h"
+#include "Position.h"
+
 /* define terminal functions */
 #define ClearTerminal() printf("\033[H\033[J")
 #define GoToTerminalPosition(x,y) printf("\033[%d;%dH", (x), (y))
@@ -31,38 +36,6 @@
 #define __ShieldObjectAppearenceLowHealth '~'
 #define __PlayerAppearence 'M'
 
-
-struct Position
-{
-  int Row;
-  int Column;
-};
-
-struct Entity
-{
-  struct Position *Position;
-  char SymbolOne;
-  char SymbolTwo;
-  bool SymbolSwitch;
-  /* use in shield object */
-  int Health;
-};
-
-struct List
-{
-  struct Entity *Entity;
-  struct List *Next;
-  struct List *Previous;
-};
-
-/* List functions */
-struct List* AddElement(struct List *ListElement, struct List *newElement);
-struct List* AllocFullListElement();
-struct List* InsertElement(struct List *ListElement, struct List *newElement);
-struct List* RemoveAndDestroyElement(struct List *removeElement);
-struct List* GetFirstElement(struct List *ListElement);
-struct List* GetLastElement(struct List *ListElement);
-int GetListCount(struct List *ListElement);
 
 /* draw support */
 void ShowSplashScreen();
@@ -192,138 +165,6 @@ void Init()
   playerPosition->Row = ((int) (terminalSize.ws_row / 20)) * 20;
 }
 
-struct List* AddElement(struct List *ListElement, struct List *newElement)
-{
-    if (ListElement == NULL)
-    {
-      newElement->Previous = NULL;
-      newElement->Next = NULL;
-      return newElement;
-    }
-    else
-    {
-        ListElement = GetLastElement(ListElement);
-        ListElement->Next = newElement;
-        ListElement->Next->Previous = ListElement;
-        ListElement->Next->Next = NULL;
-    }
-
-    return ListElement;
-}
-
-struct List* AllocFullListElement()
-{
-  struct List * listElement = (struct List*)malloc( sizeof(struct List));
-  struct Entity *entity =  (struct Entity*)malloc( sizeof(struct Entity));
-  struct Position *position =  (struct Position*)malloc( sizeof(struct Position));
-
-  /* link */
-  listElement->Entity = entity;
-  listElement->Entity->Position = position;
-
-  return listElement;
-}
-
-struct List* InsertElement(struct List *ListElement, struct List *newElement)
-{
-    if (ListElement == NULL)
-    {
-      newElement->Previous = NULL;
-      newElement->Next = NULL;
-      return newElement;
-    }
-    else
-    {
-        ListElement = GetFirstElement(ListElement);
-        ListElement->Previous = newElement;
-        ListElement->Previous->Next = ListElement;
-        ListElement->Previous->Previous = NULL;
-    }
-
-    return ListElement;
-}
-
-struct List* RemoveAndDestroyElement(struct List *removeElement)
-{
-    struct List *returnElement = NULL;
-  /* element in the middle of List */
-  if (removeElement->Next != NULL && removeElement->Previous != NULL)
-  {
-    struct List *leftElement = removeElement->Previous;
-    struct List *rightElement = removeElement->Next;
-
-    leftElement->Next = rightElement;
-    rightElement->Previous = leftElement;
-
-    returnElement = rightElement;
-  }
-  /* element stand alone */
-  else if (removeElement->Next == NULL && removeElement->Previous == NULL)
-  {
-    returnElement = NULL;
-  }
-  /* is first element */
-  else if (removeElement == GetFirstElement(removeElement))
-  {
-    struct List *rightElement = removeElement->Next;
-    rightElement->Previous = NULL;
-
-    returnElement = rightElement;
-  }
-  /* is last element */
-  else if (removeElement == GetLastElement(removeElement))
-  {
-    struct List *leftElement = removeElement->Previous;
-    leftElement->Next = NULL;
-
-    returnElement = leftElement;
-  }
-
-  free(removeElement->Entity->Position);
-  free(removeElement->Entity);
-  free(removeElement);
-  return returnElement;
-}
-
-struct List* GetFirstElement(struct List *ListElement)
-{
-  if (ListElement == NULL)
-        return NULL;
-    while (ListElement->Previous != NULL)
-    {
-        ListElement = ListElement->Previous;
-    }
-    return ListElement;
-}
-
-struct List* GetLastElement(struct List *ListElement)
-{
-  if (ListElement == NULL)
-        return NULL;
-    while (ListElement->Next != NULL)
-    {
-        ListElement = ListElement->Next;
-    }
-    return ListElement;
-}
-
-int GetListCount(struct List *ListElement)
-{
-  if (ListElement == NULL)
-    return 0;
-
-  ListElement = GetFirstElement(ListElement);
-
-  int count = 1;
-  while (ListElement->Next != NULL)
-  {
-    ListElement = ListElement->Next;
-    count++;
-  }
-
-  return count;
-}
-
 void GetNextPosition(struct Position *lastPosition, struct Position *newPosition, int listCount)
 {
   /* first invader */
@@ -347,7 +188,6 @@ void GetNextPosition(struct Position *lastPosition, struct Position *newPosition
     newPosition->Column = lastPosition->Column + __InvaderHorizontalSpace;
   }
 }
-
 void BuildShields()
 {
   int ninth = (int) terminalSize.ws_col / 9;
--- makefile
+++ makefile
@@ -1,11 +1,20 @@
-prog.bin: mkDirs obj/invader.o obj/myconio.o
-	cc -o bin/prog.bin obj/invader.o obj/myconio.o
+prog.bin: mkDirs obj/invader.o obj/myconio.o obj/List.o obj/Entity.o obj/Position.o
+	cc -o bin/prog.bin obj/invader.o obj/myconio.o obj/List.o obj/Entity.o obj/Position.o
 
 obj/invader.o: invader.c
 	cc -c invader.c -o obj/invader.o
 
 obj/myconio.o: myconio.c
 	cc -c myconio.c -o obj/myconio.o
+
+obj/List.o: List.c
+	cc -c List.c -o obj/List.o
+
+obj/Entity.o: Entity.c
+	cc -c Entity.c -o obj/Entity.o
+
+obj/Position.o: Position.c
+	cc -c Position.c -o obj/Position.o
 
 mkDirs:
 	rm -r ./obj
