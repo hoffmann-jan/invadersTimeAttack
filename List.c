f55e67da0c36201780e361b24f0f0b73fd2f678c
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2018-11-24 21:44:49 +0100

    clean whitespaces

--- List.c
+++ List.c
@@ -7,40 +7,40 @@
 
 struct List* AddElement(struct List *ListElement, struct List *newElement)
 {
-    if (ListElement == NULL)
-    {
-      newElement->Previous = NULL;
-      newElement->Next = NULL;
-      return newElement;
-    }
-    else
-    {
-        ListElement = GetLastElement(ListElement);
-        ListElement->Next = newElement;
-        ListElement->Next->Previous = ListElement;
-        ListElement->Next->Next = NULL;
-    }
+  if (ListElement == NULL)
+  {
+    newElement->Previous = NULL;
+    newElement->Next = NULL;
+    return newElement;
+  }
+  else
+  {
+    ListElement = GetLastElement(ListElement);
+    ListElement->Next = newElement;
+    ListElement->Next->Previous = ListElement;
+    ListElement->Next->Next = NULL;
+  }
 
-    return ListElement;
+  return ListElement;
 }
 
 struct List* InsertElement(struct List *ListElement, struct List *newElement)
 {
-    if (ListElement == NULL)
-    {
-      newElement->Previous = NULL;
-      newElement->Next = NULL;
-      return newElement;
-    }
-    else
-    {
-        ListElement = GetFirstElement(ListElement);
-        ListElement->Previous = newElement;
-        ListElement->Previous->Next = ListElement;
-        ListElement->Previous->Previous = NULL;
-    }
+  if (ListElement == NULL)
+  {
+    newElement->Previous = NULL;
+    newElement->Next = NULL;
+    return newElement;
+  }
+  else
+  {
+    ListElement = GetFirstElement(ListElement);
+    ListElement->Previous = newElement;
+    ListElement->Previous->Next = ListElement;
+    ListElement->Previous->Previous = NULL;
+  }
 
-    return ListElement;
+  return ListElement;
 }
 
 struct List* RemoveAndDestroyElement(struct List *removeElement)
@@ -102,23 +102,23 @@ struct List* AllocFullListElement()
 struct List* GetFirstElement(struct List *ListElement)
 {
   if (ListElement == NULL)
-        return NULL;
-    while (ListElement->Previous != NULL)
-    {
-        ListElement = ListElement->Previous;
-    }
-    return ListElement;
+    return NULL;
+  while (ListElement->Previous != NULL)
+  {
+    ListElement = ListElement->Previous;
+  }
+  return ListElement;
 }
 
 struct List* GetLastElement(struct List *ListElement)
 {
   if (ListElement == NULL)
-        return NULL;
-    while (ListElement->Next != NULL)
-    {
-        ListElement = ListElement->Next;
-    }
-    return ListElement;
+    return NULL;
+  while (ListElement->Next != NULL)
+  {
+    ListElement = ListElement->Next;
+  }
+  return ListElement;
 }
 
 int GetListCount(struct List *ListElement)
