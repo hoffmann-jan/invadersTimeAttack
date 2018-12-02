888a5e1d9ddde0a04e051d9028dc716db3ba1861
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2018-12-02 19:51:06 +0100

    add DrawTitle

--- program.h
+++ program.h
@@ -72,6 +72,7 @@ void DrawShields(Shield shield[]);
 void DrawPlayer(Player player);
 void DrawBombs(Bomb bombs[]);
 void DrawTime(int timeInSeconds);
+void DrawTitle();
 
 /* game logic*/
 void GetNextPosition(Position *lastPosition, Position *newPosition, int listCount);
